7ce70eb8b693842197c9da28a591dc97204049bb
Yixuan WANG <wang.yixuan@live.fr>
2022-12-01 19:37:57 -0500

    Add background image

--- components/Content.js
+++ components/Content.js
@@ -10,33 +10,53 @@ const Content = () => {
   const { width } = useWindowDimensions();
   const backgroundMobile = {
     backgroundImage: `url(${bgMobile.src})`,
+    backgroundPosition: "top",
+    backgroundRepeat: "no-repeat",
   };
   const backgroundDesktop = {
     backgroundImage: `url(${bgDesktop.src})`,
+    backgroundPosition: "left -7rem top 9rem",
+    backgroundSize: "contain",
+    backgroundRepeat: "no-repeat",
+    height: "39vh",
   };
   return (
-    <div
-      style={width < 960 ? backgroundMobile : backgroundDesktop}
-      className='bg-white bg-no-repeat rounded-3xl bg-top min-h-[535px]'>
-      <main className='m-0 w-[330px] min-h-[535px] flex flex-col items-center  md:flex-row md:justify-around md:w-[930px] md:min-h-[500px] md:overflow-hidden'>
-        <div>{width < 960 ? <MobileImage /> : <DesktopImage />}</div>
-        <div className='md:w-[50%] md:flex md:flex-col md:justify-start md:h-[500px]'>
-          <h1 className='text-darkBlue font-bold text-center text-4xl relative top-[-2.75rem] md:text-left md:top-[3rem]'>
-            FAQ
-          </h1>
-          <div className='flex flex-col gap-4 relative top-[-1.5rem] mb-[2.5rem] md:top-[4rem]'>
-            {Data.map((item) => (
-              <Accordion
-                key={item.id}
-                title={item.title}
-                content={item.content}
-              />
-            ))}
-          </div>
+    <main className='bg-white rounded-3xl min-h-[535px] flex m-0 w-[330px] flex-col items-center md:flex-row md:justify-around md:w-[930px] md:min-h-[500px]'>
+      <div
+        className='md:w-[50%] relative'
+        style={width < 960 ? backgroundMobile : backgroundDesktop}>
+        {width < 960 ? <MobileImage /> : <DesktopImage />}
+      </div>
+
+      <div className='md:w-[50%] md:min-h-[535px]'>
+        <h1 className='text-darkBlue font-bold text-center text-4xl relative top-[-2.75rem] md:text-left md:top-[5rem]'>
+          FAQ
+        </h1>
+        <div className='flex flex-col gap-4 relative top-[-1.5rem] mb-[2.5rem] md:top-[6rem] md:mb-0'>
+          {Data.map((item) => (
+            <Accordion
+              key={item.id}
+              title={item.title}
+              content={item.content}
+            />
+          ))}
         </div>
-      </main>
-    </div>
+      </div>
+    </main>
   );
 };
 
 export default Content;
+/*
+
+<div
+          style={width < 960 ? backgroundMobile : backgroundDesktop}
+          className='bg-no-repeat md:w-[50%]'></div>
+
+<div
+      style={width < 960 ? backgroundMobile : backgroundDesktop}
+      className='bg-white bg-no-repeat rounded-3xl min-h-[535px] flex'>
+      </div>
+
+      <main className='m-0 w-[330px] min-h-[535px] rounded-3xl flex flex-col items-center md:flex-row md:justify-around md:w-[930px] md:min-h-[500px]'>
+*/
--- components/DesktopImage.js
+++ components/DesktopImage.js
@@ -4,13 +4,19 @@ import Box from "../public/illustration-box-desktop.svg";
 
 const DesktopImage = () => {
   return (
-    <div className='p-0 m-0'>
+    <div className=''>
       <Image
-        src={ImageDesktop}
-        alt='Desktop-woman-online'
-        className='relative right-[6rem] top-0'
+        src={Box}
+        alt='box'
+        className='absolute left-[-6rem] top-[15rem] z-10'
       />
-      <Image src={Box} alt='box' className='absolute' />
+      <div className='relative overflow-hidden w-[470px] h-[535px]'>
+        <Image
+          src={ImageDesktop}
+          alt='Desktop-woman-online'
+          className='relative left-[-5rem] bottom-[-7rem]'
+        />
+      </div>
     </div>
   );
 };
