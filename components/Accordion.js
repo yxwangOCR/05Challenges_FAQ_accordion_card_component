7a8700723aa19bba82ceb71ccbe55451dff65916
Yixuan WANG <wang.yixuan@live.fr>
2022-12-02 12:26:02 -0500

    Change accordion open mode

--- components/Accordion.js
+++ components/Accordion.js
@@ -2,8 +2,11 @@ import { useState } from "react";
 import Image from "next/image";
 import Arrow from "../public/icon-arrow-down.svg";
 
-const Accordion = ({ title, content }) => {
-  const [active, setActive] = useState(false);
+const Accordion = ({ title, content, index }) => {
+  const [active, setActive] = useState(null);
+  const handleToggle = (index) => {
+    active === index ? setActive(null) : setActive(index);
+  };
 
   return (
     <div className='m-0 p-0 w-[260px] md:w-[350px]'>
@@ -11,20 +14,20 @@ const Accordion = ({ title, content }) => {
         <div className='flex justify-between items-center gap-3 my-2'>
           <p
             className={
-              active
+              active === index
                 ? "text-darkBlue text-xs font-bold cursor-pointer"
                 : "text-DarkGrayishBlue text-xs cursor-pointer"
             }
-            onClick={() => setActive(!active)}>
+            onClick={() => handleToggle(index)}>
             {title}
           </p>
 
           <Image
             src={Arrow}
             alt='arrow-icon'
-            onClick={() => setActive(!active)}
+            onClick={() => handleToggle(index)}
             className={
-              active
+              active === index
                 ? "cursor-pointer w-[10px] h-[8px] rotate-180"
                 : "cursor-pointer w-[10px] h-[8px]"
             }
@@ -32,18 +35,21 @@ const Accordion = ({ title, content }) => {
         </div>
         <hr
           className={
-            active
+            active === index
               ? "border-b-1 border-LightGrayishBlue relative top-[50px]"
               : "border-b-1 border-LightGrayishBlue relative top-[8px]"
           }></hr>
       </div>
-      {active && (
-        <p
-          className='text-DarkGrayishBlue text-[0.6rem] w-[15rem] md:w-[20rem] p-0 m-0 cursor-pointer'
-          onClick={() => setActive(!active)}>
-          {content}
-        </p>
-      )}
+
+      <p
+        onClick={() => handleToggle(index)}
+        className={
+          active === index
+            ? "text-DarkGrayishBlue text-[0.6rem] w-[15rem] md:w-[20rem] p-0 m-0 cursor-pointer"
+            : "invisible max-h-0"
+        }>
+        {content}
+      </p>
     </div>
   );
 };
--- components/Content.js
+++ components/Content.js
@@ -1,10 +1,12 @@
 import Data from "../data.json";
 import MobileImage from "./MobileImage";
 import DesktopImage from "./DesktopImage";
-import Accordion from "./Accordion";
 import useWindowDimensions from "../useWindowDimensions";
 import bgMobile from "../public/bg-pattern-mobile.svg";
 import bgDesktop from "../public/bg-pattern-desktop.svg";
+import { useState } from "react";
+import Image from "next/image";
+import Arrow from "../public/icon-arrow-down.svg";
 
 const Content = () => {
   const { width } = useWindowDimensions();
@@ -18,7 +20,11 @@ const Content = () => {
     backgroundPosition: "left -10rem top 4rem",
     backgroundSize: "cover",
     backgroundRepeat: "no-repeat",
-   
+  };
+
+  const [active, setActive] = useState(null);
+  const handleToggle = (index) => {
+    active === index ? setActive(null) : setActive(index);
   };
   return (
     <main className='bg-white rounded-3xl min-h-[535px] flex m-0 w-[330px] flex-col items-center md:flex-row md:justify-around md:w-[930px] md:min-h-[500px]'>
@@ -33,13 +39,52 @@ const Content = () => {
           FAQ
         </h1>
         <div className='flex flex-col gap-4 relative top-[-1.5rem] mb-[2.5rem] md:top-[6rem] md:mb-0'>
-          {Data.map((item) => (
-            <Accordion
-              key={item.id}
-              title={item.title}
-              content={item.content}
-            />
-          ))}
+          {Data.map((item, index) => {
+            return (
+              <div className='m-0 p-0 w-[260px] md:w-[350px]'>
+                <div className='flex flex-col'>
+                  <div className='flex justify-between items-center gap-3 my-2'>
+                    <p
+                      className={
+                        active === index
+                          ? "text-darkBlue text-xs font-bold cursor-pointer"
+                          : "text-DarkGrayishBlue text-xs cursor-pointer"
+                      }
+                      onClick={() => handleToggle(index)}>
+                      {item.title}
+                    </p>
+
+                    <Image
+                      src={Arrow}
+                      alt='arrow-icon'
+                      onClick={() => handleToggle(index)}
+                      className={
+                        active === index
+                          ? "cursor-pointer w-[10px] h-[8px] rotate-180"
+                          : "cursor-pointer w-[10px] h-[8px]"
+                      }
+                    />
+                  </div>
+                  <hr
+                    className={
+                      active === index
+                        ? "border-b-1 border-LightGrayishBlue relative top-[45px]"
+                        : "border-b-1 border-LightGrayishBlue relative top-[8px]"
+                    }></hr>
+                </div>
+
+                <p
+                  onClick={() => handleToggle(index)}
+                  className={
+                    active === index
+                      ? "text-DarkGrayishBlue text-[0.6rem] w-[15rem] md:w-[20rem] p-0 m-0 cursor-pointer"
+                      : "invisible max-h-0"
+                  }>
+                  {item.content}
+                </p>
+              </div>
+            );
+          })}
         </div>
       </div>
     </main>
